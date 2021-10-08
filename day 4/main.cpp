0c4e8401900a2c33f7f49cb159b563caf797dc12
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-10-08 21:48:28 +0200

    day 4
    basic data type

---
+++ day 4/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   char grade = 'A';
+   string learn = "C++ studying";
+   int num = 10;
+   double num1 = 20.5;
+   bool pass = true;
+
+   cout << grade << endl;
+   cout << learn << endl;
+   cout << num << endl;
+   cout << num1 << endl;
+   cout << pass << endl;
+
+    return 0;
+}
