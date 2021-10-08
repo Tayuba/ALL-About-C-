f4d05511219034f471d944a1b6b31ebe54d4d8e9
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-10-08 21:30:31 +0200

    day 3
    simple variables

---
+++ day 3/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "Ayuba";
+    int characterAge;
+    characterAge = 200;
+
+    cout << "My name is " <<characterName << endl;
+    cout << "I am " << characterAge << " years old" << endl;
+    cout << "I pray to grow to  "<< characterAge <<"  years old" << endl;
+    return 0;
+}
