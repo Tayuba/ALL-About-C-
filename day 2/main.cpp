dc6cff1d5b768f259e8eb1acf3bdc894d4ae1c45
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-10-08 21:01:59 +0200

    day 2
    Shape

---
+++ day 2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "________" << endl;
+    cout << "|      |" << endl;
+    cout << "|      |" << endl;
+    cout << "|      |" << endl;
+    cout << "|      |" << endl;
+    cout << "|______|" << endl;
+    return 0;
+}
