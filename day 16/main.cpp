367f91bde7e25ad0cd55bf1a83b1240f64478adb
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-10-10 13:32:52 +0200

    day 16
    while loop

---
+++ day 16/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cnt = 0;
+
+    while(cnt < 5)
+    {
+        cout << cnt <<" I love Zsuzsanna" << endl;
+        cnt++;
+    }
+
+    return 0;
+}
